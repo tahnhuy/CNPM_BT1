541be4282f98c8796cf67133afb48672bab6ea04
Nhat Huy <ng.nhathuy1025@gmail.com>
2026-05-04 08:37:25 +0700

    project commit

---
+++ src/config/configdb.js
@@ -0,0 +1,18 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('node_fulltask', 'root', '1234567@a$', {
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    logging: false
+});
+
+let connectDB = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+}
+
+module.exports = connectDB;
